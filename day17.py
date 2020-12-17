ddbe34e2aa27243f861ddc329b261e2030c35802
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2020-12-17 20:13:13 +0100

    improve

--- day17.py
+++ day17.py
@@ -1,7 +1,6 @@
 import sys
 
 lines = open("%s_input.txt" % sys.argv[0].split('.')[0], "r")
-
 actives = []
 actives4 = []
 
@@ -12,53 +11,33 @@ for x, line in enumerate(lines):
             actives4.append((x, y, 0, 0))
 
 def neighbours(x, y, z):
-    n = []
-    for x_offset in range(-1, 2):
-        for y_offset in range(-1, 2):
-            for z_offset in range(-1, 2):
-                n.append((x + x_offset, y + y_offset, z + z_offset))
-    return n
+    return [(x + x_offset, y + y_offset, z + z_offset)
+            for x_offset in range(-1, 2)
+            for y_offset in range(-1, 2)
+            for z_offset in range(-1, 2)]
 
-def neighbours4(x, y, z, w):
-    n = []
-    for x_offset in range(-1, 2):
-        for y_offset in range(-1, 2):
-            for z_offset in range(-1, 2):
-                for w_offset in range(-1, 2):
-                    n.append((x + x_offset, y + y_offset, z + z_offset, w + w_offset))
-    return n
+def neighbours4(x, y, z, w):    
+    return [(x + x_offset, y + y_offset, z + z_offset, w + w_offset)
+            for x_offset in range(-1, 2)
+            for y_offset in range(-1, 2)
+            for z_offset in range(-1, 2)
+            for w_offset in range(-1, 2)]
 
-# 6 tours
-for i in range(6):
-    print("# Tour %d" % i)
-    print("3d: %d, 4d: %d" % (len(actives), len(actives4)))
-
+def process(actives, neighbours_function):
     new_actives = []
-
     for active_cell in actives:
-        #print("active_cell: ", active_cell)
-        for cell in neighbours(*active_cell):        
-            #print("cell: ", cell)
-            nb_active_neighours = len(list(set(actives) & set(neighbours(*cell))))
+        for cell in neighbours_function(*active_cell):        
+            nb_active_neighours = len(list(set(actives) & set(neighbours_function(*cell))))
             if (cell not in new_actives
                 and ((cell in actives and 3 <= nb_active_neighours <= 4)
                 or (cell not in actives and nb_active_neighours == 3))):
-
                 new_actives.append(cell)            
-    actives = new_actives
+    return new_actives
 
-    new_actives4 = []
-    for active_cell in actives4:
-        #print("active_cell: ", active_cell)
-        for cell in neighbours4(*active_cell):        
-            #print("cell: ", cell)
-            nb_active_neighours = len(list(set(actives4) & set(neighbours4(*cell))))
-            if (cell not in new_actives4
-                and ((cell in actives4 and 3 <= nb_active_neighours <= 4)
-                or (cell not in actives4 and nb_active_neighours == 3))):
-
-                new_actives4.append(cell)
-    actives4 = new_actives4
+for i in range(6):
+    print("Tour %d" % i)
+    actives = process(actives, neighbours)
+    actives4 = process(actives4, neighbours4)
 
 print("ex1: %s" % len(actives))
 print("ex2: %s" % len(actives4))
