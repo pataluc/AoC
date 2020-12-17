08e241eb4f08d4508ea0e0f90fabcbeed151a941
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2020-12-17 22:51:29 +0100

    optim

--- day17.py
+++ day17.py
@@ -1,37 +1,40 @@
 import sys
 
 lines = open("%s_input.txt" % sys.argv[0].split('.')[0], "r")
-actives = []
-actives4 = []
+actives = set()
+actives4 = set()
 
 for x, line in enumerate(lines):
     for y, cell in enumerate((line.rstrip())):
         if cell == '#':
-            actives.append((x, y, 0))
-            actives4.append((x, y, 0, 0))
+            actives.add((x, y, 0))
+            actives4.add((x, y, 0, 0))
 
 def neighbours(x, y, z):
-    return [(x + x_offset, y + y_offset, z + z_offset)
+    return set((x + x_offset, y + y_offset, z + z_offset)
             for x_offset in range(-1, 2)
             for y_offset in range(-1, 2)
-            for z_offset in range(-1, 2)]
+            for z_offset in range(-1, 2))
 
 def neighbours4(x, y, z, w):    
-    return [(x + x_offset, y + y_offset, z + z_offset, w + w_offset)
+    return set((x + x_offset, y + y_offset, z + z_offset, w + w_offset)
             for x_offset in range(-1, 2)
             for y_offset in range(-1, 2)
             for z_offset in range(-1, 2)
-            for w_offset in range(-1, 2)]
+            for w_offset in range(-1, 2))
 
 def process(actives, neighbours_function):
-    new_actives = []
-    for active_cell in actives:
-        for cell in neighbours_function(*active_cell):        
-            nb_active_neighours = len(list(set(actives) & set(neighbours_function(*cell))))
-            if (cell not in new_actives
-                and ((cell in actives and 3 <= nb_active_neighours <= 4)
-                or (cell not in actives and nb_active_neighours == 3))):
-                new_actives.append(cell)            
+    new_actives = set()
+    actives_and_neighbours = set()
+    for cell in actives:
+        actives_and_neighbours = actives_and_neighbours.union(neighbours_function(*cell))
+
+    for cell in actives_and_neighbours:        
+        nb_active_neighours = len(actives & neighbours_function(*cell))
+        if (cell not in new_actives
+            and ((cell in actives and 3 <= nb_active_neighours <= 4)
+            or (cell not in actives and nb_active_neighours == 3))):
+            new_actives.add(cell)            
     return new_actives
 
 for i in range(6):
@@ -41,5 +44,4 @@ for i in range(6):
 
 print("ex1: %s" % len(actives))
 print("ex2: %s" % len(actives4))
-
 
