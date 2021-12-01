9940d6dec7efeed905220e6f68bf2e219113e238
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2021-12-01 18:08:54 +0100

    improvements ;)

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -2,31 +2,17 @@ import sys
 file = "sample.txt" if len(sys.argv) == 2 and sys.argv[1] == "-d" else "input.txt"
 
 def file_to_array():
-    f = open(file, "r")
-    depths = []
-    for line in f:
-        depths.append(int(line))
-    
-    return depths
+    return [int(line) for line in open(file, "r").readlines()]
 
 def count_increased(array):
-    inc_count = None
-    last_value = 0
-    for value in array:
-        if inc_count is None:
-            inc_count = 0
-        elif value > last_value:
-            inc_count += 1
-        
-        last_value = value
-    return inc_count
+    return sum([1 for i in range(len(array)) if array[i] > array[i-1]])
 
 def ex1():
     return count_increased(file_to_array())
 
 def ex2():
     depths = file_to_array()
-    return count_increased(map(sum, zip(depths, depths[1:], depths[2:])))
+    return count_increased(list(map(sum, zip(depths, depths[1:], depths[2:]))))
 
 
 print("ex1 : %d" % ex1())
