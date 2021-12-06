72abc1b4f4d359d09e8a05b55412e23ec59aa24f
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2021-12-06 15:00:55 +0100

    improved day5

--- 2021/day5/day5.py
+++ 2021/day5/day5.py
@@ -11,32 +11,28 @@ def print_grid(grid):
     for line in grid:
         print("".join(map(lambda x : str(x) if x > 0 else '.', line)))
 
-def apply_vent(grid, vent):
-    y1, x1, y2, x2 = vent
-    dir_x = 1 if x2 >= x1 else -1
-    dir_y = 1 if y2 >= y1 else -1
+def improved_range(i, j):
+    dir = 1 if j >= i else -1
+    return range(i, j + dir, dir)
 
-    if x1 == x2: #horiz
-        for y in range(min(y1, y2), max(y1, y2) + 1):
-            grid[x1][y] += 1 
-    elif y1 == y2: #vertical
-        for x in range(min(x1, x2), max(x1, x2) + 1):
-            grid[x][y1] += 1         
-    else: #diag
-        for i, j in zip(range(0, dir_x + x2 - x1, dir_x), range(0, dir_y + y2 - y1, dir_y)):
-            grid[x1 + i][y1 + j] += 1               
+def get_points(vent):    
+    x1, y1, x2, y2 = vent
+    return list(zip(
+        improved_range(x1, x2) if x1 != x2 else [x1] * (abs(y2 - y1) + 1),
+        improved_range(y1, y2) if y1 != y2 else [y1] * (abs(x2 - x1) + 1)
+    ))
+
+def count_overlaps(vents):
+    occurences = {}
+    for p in sum(list(map(get_points, vents)), []):
+        occurences[p] = occurences.get(p, 0) + 1
+    return len(list(filter(lambda x : x[1] >= 2, occurences.items())))              
 
 def ex1():
-    grid = [[0 for i in range(width + 1)] for j in range(width + 1)]
-    for vent in list(filter(lambda x : x[0] == x[2] or x[1] == x[3], vents)):
-        apply_vent(grid, vent)
-    return sum(map(lambda line: len(list(filter(lambda x: x >= 2, line))), grid))
+    return count_overlaps(list(filter(lambda x : x[0] == x[2] or x[1] == x[3], vents)))
 
 def ex2():
-    grid = [[0 for i in range(width + 1)] for j in range(width + 1)]
-    for vent in list(filter(lambda x : x[0] == x[2] or x[1] == x[3] or abs(x[0] - x[3]) == abs(x[1] - x[2]) or abs(x[0] - x[2]) == abs(x[1] - x[3]), vents)):
-        apply_vent(grid, vent)
-    return sum(map(lambda line: len(list(filter(lambda x: x >= 2, line))), grid))
+    return count_overlaps(vents)
 
 print("ex1 : %d" % ex1())
 print("ex2 : %d" % ex2())
