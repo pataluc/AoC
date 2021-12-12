ef8d984c0c8b3ebe2ef19d6d4d245afd06777cc8
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2021-12-12 22:50:54 +0100

    day12

--- 2021/day12/day12.py
+++ 2021/day12/day12.py
@@ -1,127 +1,70 @@
-from collections import defaultdict
-  
-# This class represents a directed graph
-# using adjacency list representation
-class Graph:
-
-  
-    def __init__(self, vertices):
-        # No. of vertices
-        self.V = len(vertices)
-        self.points = vertices
-         
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
-  
-    # function to add an edge to graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
-  
-    '''A recursive function to print all paths from 'u' to 'd'.
-    visited[] keeps track of vertices in current path.
-    path[] stores actual vertices and path_index is current
-    index in path[]'''
-    def printAllPathsUtil(self, u, d, visited, path, paths = []):
-        
- 
-        # Mark the current node as visited and store in path
-        visited[u] = not self.points[u].isupper()
-        path.append(u)
- 
-        # If current vertex is same as destination, then print
-        # current path[]
-        if u == d and sum(map(lambda x: 1 if "2" in self.points[x] else 0, path)) <= 1:
-
-            #print(",".join(map(lambda x: self.points[x], path)).replace("2", ""))
-            paths.append(",".join(map(lambda x: self.points[x], path)).replace("2", ""))
-        else:
-            # If current vertex is not destination
-            # Recur for all the vertices adjacent to this vertex
-            for i in self.graph[u]:
-                if visited[i] == False:
-                    self.printAllPathsUtil(i, d, visited, path)
-                     
-        # Remove current vertex from path[] and mark it as unvisited
-        path.pop()
-        visited[u] = False
-        return paths
-  
-  
-    # Prints all paths from 's' to 'd'
-    def printAllPaths(self, s, d):
- 
-        # Mark all the vertices as not visited
-        visited = [False]*(self.V)
- 
-        # Create an array to store paths
-        path = []
- 
-        # Call the recursive helper function to print all paths
-        c = self.printAllPathsUtil(s, d, visited, path)
-        return c
-  
-  
-  
 def load(file):
     return [ tuple(l.rstrip().split("-")) for l in open(file, "r").readlines()]
 
-def get_paths(input):
-    points = list(set(map(lambda x: x[0], input)).union(set(map(lambda x: x[1], input))))
-    points.sort()
-        
-    g = Graph(points)
-    #print(points, len(points))
-    for a, b in input:
-        g.addEdge(points.index(a), points.index(b))
-        g.addEdge(points.index(b), points.index(a))
-        
-    #print(g.graph)
-    #print("Following are all different paths from start to end :")
-    return g.printAllPaths(points.index("start"), points.index("end"))
+def is_valid_neighbour1(new_node, path):
+    return new_node not in path
 
-def get_paths2(input):
-    points = list(set(map(lambda x: x[0], input)).union(set(map(lambda x: x[1], input))))
-    points += list(map(lambda x: "%s2" % x,  filter(lambda x: x not in ['start', 'end'] and x.islower(), points)))    
-    points.sort()
-        
-    g = Graph(points)
-    #print(points, len(points))
-    for a, b in input:
-        g.addEdge(points.index(a), points.index(b))
-        g.addEdge(points.index(b), points.index(a))
-        if a not in ['start', 'end'] and a.islower():
-            g.addEdge(points.index("%s2" % a), points.index(b))
-            g.addEdge(points.index(b), points.index("%s2" % a))
-        if b not in ['start', 'end'] and b.islower():
-            g.addEdge(points.index(a), points.index("%s2" % b))
-            g.addEdge(points.index("%s2" % b), points.index(a))
+def is_valid_neighbour2(new_node, path):
+    small_caves_freq = {}
+    for node in path:
+        if node.isupper():
+            continue
+        if node not in small_caves_freq:
+            small_caves_freq[node] = 0
+        small_caves_freq[node] += 1
+    if new_node not in small_caves_freq:
+        return True
+    if new_node == "start" or new_node == "end":
+        return False
+    return len(small_caves_freq) == sum(small_caves_freq.values())
 
-        
-    #print(g.graph)
-    #print("Following are all different paths from start to end :")
-    return g.printAllPaths(points.index("start"), points.index("end"))
+def solve(lines, is_valid_neighbour):
+    edges = {}
+    for i, j in lines:
+        if i not in edges:
+            edges[i] = set()
+        edges[i].add(j)
+        if i != "start" and j != "end":
+            if j not in edges:
+                edges[j] = set()
+            edges[j].add(i)
+    
+    paths = [['start']]
+    while True:
+        print(paths)
+        new_paths = []
+        for path in paths:
+            last_node = path[-1]
+            if last_node == 'end' or last_node not in edges:
+                new_paths.append(path)
+                continue
 
+            for neighbour in edges[last_node]:
+                if neighbour.islower() and not is_valid_neighbour(neighbour, path):
+                    continue
 
-def ex1(lines):    
-    return get_paths(lines)
+                new_paths.append(path + [neighbour])
+    
+        if len(paths) == len(new_paths):
+            break
 
-def ex2(lines):
-    c = set(get_paths2(lines))
-    print(c)
-    print(len(c))
-    return len(c)
+        paths = new_paths
+
+    #print(len(paths))
+    return len(paths)
 
 sample = load("sample.txt")
 sample1 = load("sample1.txt")
 sample2 = load("sample2.txt")
 input = load("input.txt")
 
-#assert ex1(sample) == 10
-#assert ex1(sample1) == 19
-#assert ex1(sample2) == 226
-#print("ex1 : %d" % ex1(input))
+assert solve(sample, is_valid_neighbour1) == 10
+exit()
+assert solve(sample1, is_valid_neighbour1) == 19
+assert solve(sample2, is_valid_neighbour1) == 226
+print("ex1 : %d" % solve(input, is_valid_neighbour1))
 
-#assert ex2(sample) == 36
-#assert ex2(sample1) == 103
-#assert ex2(sample2) == 3509
-print("ex1 : %d" % ex2(input))
+assert solve(sample, is_valid_neighbour2) == 36
+assert solve(sample1, is_valid_neighbour2) == 103
+assert solve(sample2, is_valid_neighbour2) == 3509
+print("ex2 : %d" % solve(input, is_valid_neighbour2))
