fce0b710057de811d95aa37a60d54b960d62eebb
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2021-12-13 14:20:06 +0100

    day13

---
+++ 2021/day13/day13.py
@@ -0,0 +1,44 @@
+from types import new_class
+
+
+def load(file):
+    lines = open(file, "r").readlines()
+    sep = lines.index("\n")
+    return [tuple(map(int, line.strip().split(','))) for line in lines[:sep]], \
+        list(map(lambda x: (x[0], int(x[1])), map(lambda x: x.strip().replace("fold along ", "").split('='), lines[sep+1:])))
+
+def solve(dots, folds):
+    for fold in folds:
+        new_dots = set()
+
+        for dot in dots:
+            if fold[0] == "x":
+                new_dots.add((abs(dot[0] - 2*fold[1]) if dot[0] > fold[1] else dot[0], dot[1]))
+            else:
+                new_dots.add((dot[0], abs(dot[1] - 2*fold[1]) if dot[1] > fold[1] else dot[1]))  
+        dots = new_dots
+    return dots
+
+
+def ex1(dots, folds):
+    return len(solve(dots, folds[:1]))
+
+def ex2(dots, folds):
+    dots = solve(dots, folds)
+
+    r = [[' '] * 50 for _ in range(6)]
+    for dot in dots:
+        r[dot[1]][dot[0]] = "#"
+
+    for rr in r:
+        print("".join(rr))
+
+    return dots
+
+sample = load("sample.txt")
+assert ex1(sample[0], sample[1]) == 17
+
+input = load("input.txt")
+print("ex1 : %d" % ex1(input[0], input[1]))
+print("ex2 :")
+ex2(input[0], input[1])
