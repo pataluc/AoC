60e4fb6c52c20b450abb00680e9f4e1e4fadb809
Luc Fouin <luc.fouin@gmail.com>
2021-12-13 18:35:56 +0100

    taille auto

--- 2021/day13/day13.py
+++ 2021/day13/day13.py
@@ -8,25 +8,29 @@ def load(file):
         list(map(lambda x: (x[0], int(x[1])), map(lambda x: x.strip().replace("fold along ", "").split('='), lines[sep+1:])))
 
 def solve(dots, folds):
+    last_x, last_y = (0, 0)
+
     for fold in folds:
         new_dots = set()
 
         for dot in dots:
             if fold[0] == "x":
                 new_dots.add((abs(dot[0] - 2*fold[1]) if dot[0] > fold[1] else dot[0], dot[1]))
+                last_x = fold[1]
             else:
                 new_dots.add((dot[0], abs(dot[1] - 2*fold[1]) if dot[1] > fold[1] else dot[1]))  
+                last_y = fold[1]
         dots = new_dots
-    return dots
+    return dots, last_x, last_y
 
 
 def ex1(dots, folds):
-    return len(solve(dots, folds[:1]))
+    return len(solve(dots, folds[:1])[0])
 
 def ex2(dots, folds):
-    dots = solve(dots, folds)
+    dots, width, length = solve(dots, folds)
 
-    r = [[' '] * 50 for _ in range(6)]
+    r = [[' '] * width for _ in range(length)]
     for dot in dots:
         r[dot[1]][dot[0]] = "#"
 
