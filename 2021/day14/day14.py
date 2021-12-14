d7ccd6bb923552112ee13759709d29c16f9c2589
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2021-12-14 11:09:05 +0100

    day14 ex2

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -1,50 +1,59 @@
-from types import new_class
-
 
 def load(file):
     lines = open(file, "r").readlines()
     return lines[0].strip(), dict(map(lambda x: tuple(x.strip().split(" -> ")), lines[2:]))
 
 def process(template, instructions):
-    #print("Process template %s with inscructions " % template, instructions)
-    result = template[0]
-    for i in range(len(template) - 1):
-        #print(template[i:i+2])
-        #print(template[i+1])
-        if template[i:i+2] in instructions:
-            result += instructions[template[i:i+2]]
-        result += template[i+1]
-    #print(result)
-    return result
-
-
-def ex1(input, rounds = 1):
-    r = input[0]
-    for i in range(rounds):
-        print("round %d, size template: %d" % (i, len(r)))
-        r = process(r, input[1])
-
-    #print(r)
-    l = list(r)
-    # empty dictionary to hold pair of number and its count
     d = {}
-    # loop through all elements and store count
-    [ d.update( {i:d.get(i, 0)+1} ) for i in l ]
+    for k in template.keys():
+        if k in instructions:
+            if k[0] + instructions[k] in d:
+                d[k[0] + instructions[k]] += template[k]
+            else:
+                d[k[0] + instructions[k]] = template[k]
+            if instructions[k] + k[1] in d:
+                d[instructions[k] + k[1]] += template[k]
+            else:
+                d[instructions[k] + k[1]] = template[k]
+        else:
+            d[k] = template[k]
 
-    v = list(d.values())
-    v.sort()
-    return v[-1]-v[0]
+    return d
 
-def ex2(dots, folds):
-    return 0
+
+def ex(input, rounds = 1):
+    template, intructions = input
+    
+    t = {}
+    for i in range(len(template) - 1):
+        if template[i:i+2] in t:
+            t[template[i:i+2]] += 1
+        else:
+            t[template[i:i+2]] = 1
+
+    for i in range(rounds):
+        t = process(t, intructions)
+
+    r = {}
+    for k in t.keys():
+        n = t[k]
+        for c in k:
+            if c in r:
+                r[c] += n
+            else:
+                r[c] = n
+    r[template[0]] += 1
+    r[template[-1]] += 1
+
+    l = list(r.values())
+    l.sort()
+    return ((l[-1]-l[0]) // 2)
 
 sample = load("sample.txt")
-assert process(sample[0], sample[1]) == 'NCNBCHB'
-assert process(process(sample[0], sample[1]), sample[1]) == 'NBCCNBBBCBHCB'
-assert ex1(sample, 10) == 1588
+assert ex(sample, 10) == 1588
 
 input = load("input.txt")
-print("ex1 : %d" % ex1(input, 10))
+print("ex1 : %d" % ex(input, 10))
 
-assert ex1(sample, 40) == 2188189693529
-print("ex2 : %d" % ex1(input, 40))
+assert ex(sample, 40) == 2188189693529
+print("ex2 : %d" % ex(input, 40))
