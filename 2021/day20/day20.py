5f705f9249b916a3c3bb2d5c256f0b0c83d57ea0
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2021-12-20 18:57:19 +0100

    day20 improved

--- 2021/day20/day20.py
+++ 2021/day20/day20.py
@@ -1,12 +1,11 @@
-from itertools import permutations, product
-from os import X_OK, path
+from os import path
 from sys import argv
 
 
 
 def load(file):
     input = open(file, "r").read().split("\n\n")
-    image = []
+    image = set()
     i = 0
     for line in input[1].split("\n"):
         i += 1
@@ -14,35 +13,35 @@ def load(file):
         for c in line:
             j += 1
             if c == "#":
-                image.append((i,j))
+                image.add((i,j))
     return image, input[0]
 
 def enhance(image, algo, default, framesize):
     if framesize:
         min_x, max_x, min_y, max_y = framesize
     else:
-        xs = list(map(lambda x: x[0], image))
-        min_x, max_x = min(xs), max(xs)
-        ys = list(map(lambda x: x[1], image))
-        min_y, max_y = min(ys), max(ys)
+        min_x = min([r for r, _ in image])
+        max_x = max([r for r, _ in image])
+        min_y = min([c for _, c in image])
+        max_y = max([c for _, c in image])
 
-    new_image = []
+    new_image = set()
     for i in range(min_x - 2, max_x + 3):
         for j in range(min_y - 2, max_y + 3):
-            s = ""
+            index = 0
+            bit = 8
             #print("---", i, j)
             #print_image(image, (i - 1, i + 2, j - 1, j+ 2))
-            for m in (-1, 0, 1):
-                for n in (-1, 0, 1):
+            for m in [-1, 0, 1]:
+                for n in [-1, 0, 1]:
                     if (i + m, j + n) in image or (
                         (not (min_x <= i + m <= max_x) or not(min_y <= j + n <= max_y)) and default == "#"
                         ):
-                        s += '1'
-                    else:
-                        s += '0'
+                        index += 2**bit
+                    bit -= 1
             #print("%s = %d, algo: %s" % (s, int(s, 2), algo[int(s, 2)]))
-            if algo[int(s, 2)] == "#":
-                new_image.append((i, j))
+            if algo[index] == "#":
+                new_image.add((i, j))
                 
     return new_image, (min_x - 1, max_x + 1, min_y - 1, max_y + 1)
 
@@ -71,7 +70,7 @@ def ex1(image, algo):
     framesize = None
     #print(len(image))
     #print_image(image, default)
-    for _ in range (2):
+    for _ in range(2):
         image, framesize = enhance(image, algo, default, framesize)
         if algo[0] == "#":
             default = '.' if default == "#" else "#"
@@ -81,13 +80,14 @@ def ex1(image, algo):
 
 def ex2(image, algo):
     default = '.'
+    framesize = None
     #print(len(image))
     #print_image(image, default)
-    for i in range (50):
-        image = enhance(image, algo, default)
+    for i in range(50):
+        image, framesize = enhance(image, algo, default, framesize)
         if algo[0] == "#":
             default = '.' if default == "#" else "#"
-        print(i, len(image))
+        #print(i, len(image))
         #print_image(image, default)
     return len(image)
 
@@ -99,10 +99,9 @@ assert r == 35
 image, algo = load("%s/input.txt" % (path.dirname(argv[0]) if path.dirname(argv[0]) else "."))
 print("ex1 : %d" % ex1(image, algo))
 
-exit()
 image, algo = load("%s/sample.txt" % (path.dirname(argv[0]) if path.dirname(argv[0]) else "."))
-#r = ex2(image, algo)
-#assert r == 3351
+r = ex2(image, algo)
+assert r == 3351
 
 image, algo = load("%s/input.txt" % (path.dirname(argv[0]) if path.dirname(argv[0]) else "."))
 print("ex2 : %d" % ex2(image, algo))
