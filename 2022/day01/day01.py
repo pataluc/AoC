13e16b993cdbdf05c43ea216b1b2918310774c1f
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2022-12-01 11:50:09 +0100

    linting...

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -1,26 +1,28 @@
 from os import path
 from sys import argv
 
+def file_path(file):
+    return "%s/%s" % (path.dirname(argv[0]) if path.dirname(argv[0]) else ".", file)
+
 def load(file):
-    input = list(map(lambda x: [int(n) for n in x.split("\n")], open(file, "r").read().split("\n\n")))
+    input = list(map(lambda x: [int(n) for n in x.split("\n")], open(file_path(file), "r").read().split("\n\n")))
 
     return input
 
+def solve(calories, count):
+    return sum(sorted(map(sum, calories), reverse=True)[:count])
+
 def ex1(calories):
-    return max(map(sum, calories))
+    return solve(calories, 1)
 
 def ex2(calories):
-    return sum(sorted(map(sum, calories))[-3:])
+    return solve(calories, 3)
 
 
-calories = load("%s/sample.txt" % (path.dirname(argv[0]) if path.dirname(argv[0]) else "."))
-r = ex1(calories)
-assert r == 24000
-r = ex2(calories)
-assert r == 45000
+calories = load("sample.txt")
+assert ex1(calories) == 24000
+assert ex2(calories) == 45000
 
-calories = load("%s/input.txt" % (path.dirname(argv[0]) if path.dirname(argv[0]) else "."))
-r = ex1(calories)
-print("ex1 : %d" % r)
-r = ex2(calories)
-print("ex2 : %d" % r)
+calories = load("input.txt")
+print("ex1 : %d" % ex1(calories))
+print("ex2 : %d" % ex2(calories))
