8c42d2f517c0b4c7dab9949d90c1f3541efb8552
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2022-12-14 09:41:15 +0100

    days 13 and 14

---
+++ 2022/day13/day13.py
@@ -0,0 +1,113 @@
+import re
+from os import path
+from sys import argv
+import numpy as np
+from collections import defaultdict
+import functools
+
+def file_path(file):
+    return "%s/%s" % (path.dirname(argv[0]) if path.dirname(argv[0]) else ".", file)
+
+def load(file):
+    return [ list(map(eval, l.split("\n"))) for l in open(file_path(file), "r").read().split("\n\n") ]
+
+def load2(file):
+    return [ eval(l) for l in open(file_path(file), "r").read().split("\n") if l != '' ] + [[[2]]] + [[[6]]]
+
+debug = False
+
+def compare(l, m, prefix = ""):
+    # listes non vide
+    if len(l) and len(m):
+        i = 0
+        while i < min(len(l), len(m)):
+            n, o = l[i], m[i]
+            print("  %s- Compare" % prefix, n, "vs", o) if debug else None
+            if isinstance(n, int) and isinstance(o, int):
+                if n < o:
+                    print("    %s- Left side is smaller, so inputs are in the right order" % prefix) if debug else None
+                    return True
+                elif n > o:
+                    print("    %s- Right side is smaller, so inputs are not in the right order" % prefix) if debug else None
+                    return False
+            elif isinstance(n, list) and isinstance(o, list):
+                if len(n) and len(o):
+                    r = compare(n, o, prefix + "  ")
+                    if r != None:
+                        return r
+                elif len(o):
+                    print("    %s- Left side ran out of items, so inputs are in the right order" % prefix) if debug else None
+                    return True
+                elif len(n):
+                    print("    %s- Right side ran out of items, so inputs are not in the right order" % prefix) if debug else None
+                    return False
+            elif isinstance(n, list):
+                print("    %s- Mixed types; convert right to [%d] and retry comparison" % (prefix, o)) if debug else None
+                print("    %s- Compare" % prefix, n, "vs", [o]) if debug else None
+                r = compare(n, [o], prefix + "    ")
+                if r != None:
+                    return r
+            elif isinstance(o, list):
+                print("    %s- Mixed types; convert left to [%d] and retry comparison" % (prefix, n)) if debug else None
+                print("    %s- Compare" % prefix, [n], "vs", o) if debug else None
+                r = compare([n], o, prefix + "    ")
+                if r != None:
+                    return r
+            i += 1
+
+        if i == len(l) and i < len(m):
+            print("  %s- Left side ran out of items, so inputs are in the right order" % prefix) if debug else None
+            return True
+        elif i < len(l) and i == len(m):
+            print("  %s- Right side ran out of items, so inputs are not in the right order" % prefix) if debug else None
+            return False
+
+    elif len(m):
+        print("  %s- Left side ran out of items, so inputs are in the right order" % prefix) if debug else None
+        return True
+    elif len(l):
+        print("  %s- Right side ran out of items, so inputs are not in the right order" % prefix) if debug else None
+        return False
+    else:
+        return None
+
+def my_compare(l, m):
+    return -1 if compare(l, m) else 1
+
+def ex1(pairs):
+    score = 0
+    ok_pairs = []
+    for i, [l, m] in enumerate(pairs):
+        print("== Pair %d ==" % (i + 1)) if debug else None
+        print("- Compare", l, "vs", m) if debug else None
+
+        if compare(l, m):
+            print("Pair %d is OK") if debug else None
+            score += (i + 1)
+            ok_pairs.append(i + 1)
+        print("") if debug else None
+    return score
+
+
+
+def ex2(pairs):
+    pairs = sorted(pairs, key=functools.cmp_to_key(my_compare))
+
+    return (pairs.index([[2]]) + 1) * (pairs.index([[6]]) + 1)
+
+sample1 = load("sample.txt")
+
+# print(compare([1,1,3,1,1], [1,1,5,1,1]))
+# exit()
+# print(sample1)
+assert ex1(sample1) == 13
+
+data = load("input.txt")
+print("ex1 : %s" % ex1(data))
+
+
+sample1 = load2("sample.txt")
+data = load2("input.txt")
+
+assert ex2(sample1) == 140
+print("ex2 : %s" % ex2(data))
---
+++ 2022/day14/day14.py
@@ -0,0 +1,137 @@
+import re
+from os import path
+from sys import argv
+import numpy as np
+from collections import defaultdict
+import functools
+
+def file_path(file):
+    return "%s/%s" % (path.dirname(argv[0]) if path.dirname(argv[0]) else ".", file)
+
+def myrange(x, y):
+    if x <= y:
+        return range(x, y + 1)
+    else:
+        return range(y, x + 1)
+
+def load(file):
+    lines = [ list(map(eval, l.split(" -> "))) for l in open(file_path(file), "r").read().split("\n") ]
+    minx = lines[0][0][0]
+    maxx = lines[0][0][0]
+    miny = lines[0][0][1]
+    maxy = lines[0][0][1]
+    rocks = set()
+    for line in lines:
+        for point in line:
+            if point[0] < minx:
+                minx = point[0]
+            if point[0] > maxx:
+                maxx = point[0]
+            if point[1] < miny:
+                miny = point[1]
+            if point[1] > maxy:
+                maxy = point[1]
+        for i, point in enumerate(line[:-1]):
+            for x in myrange(point[0], line[i+1][0]):
+                for y in myrange(point[1], line[i+1][1]):
+                    rocks.add((x, y))
+    # print(minx, maxx, miny, maxy)
+    # print(rocks)
+    return (rocks, minx, maxx, miny, maxy)
+
+debug = False
+def dprint(s):
+    if debug:
+        print(s)
+
+def display(data, sand, current_sand, has_floor = False):
+    if not debug:
+        return
+    rocks, minx, maxx, miny, maxy = data
+    if has_floor:
+        minx = min(map(lambda p: p[0], rocks.union(sand)))
+        maxx = max(map(lambda p: p[0], rocks.union(sand)))
+    print("   %d%s%d" % (minx, " " * (maxx-minx-4), maxx))
+    for y in range(maxy + 2):
+        print("%3d %s" % (y, "".join([ 'o' if (x, y) in sand or (x, y) == current_sand else "#" if (x, y) in rocks else '+' if (x, y) == (500,0) else "." for x in range(minx, maxx + 1) ])))
+    if has_floor:
+        print("%3d %s" % (y + 1, "".join([ "#" * (maxx - minx + 1) ])))
+
+def can_flow(sand_position, rocks, sand):
+    return ((sand_position[0], sand_position[1] + 1) not in rocks and (sand_position[0], sand_position[1] + 1) not in sand) \
+        or ((sand_position[0] - 1, sand_position[1] + 1) not in rocks and (sand_position[0] - 1, sand_position[1] + 1) not in sand) \
+        or ((sand_position[0] + 1, sand_position[1] + 1) not in rocks and (sand_position[0] + 1, sand_position[1] + 1) not in sand)
+
+def ex1(data):
+    source = (500, 0)
+    sand = set()
+    rocks, minx, maxx, miny, maxy = data
+    # display(data, sand) 
+
+    keep_producing = True
+    while keep_producing:
+        sand_position = source
+
+        while can_flow(sand_position, rocks, sand) and (sand_position[1] < maxy):
+            if ((sand_position[0], sand_position[1] + 1) not in rocks and (sand_position[0], sand_position[1] + 1) not in sand):
+                sand_position = (sand_position[0], sand_position[1] + 1)
+            elif ((sand_position[0] - 1, sand_position[1] + 1) not in rocks and (sand_position[0] - 1, sand_position[1] + 1) not in sand):
+                sand_position = (sand_position[0] - 1, sand_position[1] + 1)
+            else:
+                sand_position = (sand_position[0] + 1, sand_position[1] + 1)
+
+        if (sand_position[1] < maxy):
+            sand.add(sand_position)
+        # display(data, sand)
+
+        keep_producing = (sand_position[1] < maxy)
+
+    # display(data, sand)
+    return len(sand)
+
+
+
+def ex2(data):
+    source = (500, 0)
+    sand = set()
+    rocks, minx, maxx, miny, maxy = data
+    dprint("\n Initial state")
+    display(data, sand, None, True) 
+
+    dprint("\nProducing sand")
+    keep_producing = True
+    while keep_producing:
+        # dprint("production sand particle number %d" % (len(sand) + 1))
+        sand_position = source
+
+        while can_flow(sand_position, rocks, sand) and (sand_position[1] < (maxy + 2)):
+            if ((sand_position[0], sand_position[1] + 1) not in rocks and (sand_position[0], sand_position[1] + 1) not in sand):
+                sand_position = (sand_position[0], sand_position[1] + 1)
+            elif ((sand_position[0] - 1, sand_position[1] + 1) not in rocks and (sand_position[0] - 1, sand_position[1] + 1) not in sand):
+                sand_position = (sand_position[0] - 1, sand_position[1] + 1)
+            else:
+                sand_position = (sand_position[0] + 1, sand_position[1] + 1)
+            # display(data, sand, sand_position, True)
+        
+        sand.add(sand_position)
+        # display(data, sand, None, True)
+
+        keep_producing = (sand_position != source)
+    display(data, sand, None, True)
+
+
+    # print(len(sand))
+    # print(sand)
+
+    return len(list(filter(lambda p: p[1] < maxy + 2, sand)))
+
+sample1 = load("sample.txt")
+
+assert ex1(sample1) == 24
+
+data = load("input.txt")
+print("ex1 : %s" % ex1(data))
+
+
+assert ex2(sample1) == 93
+print("ex2 : %s" % ex2(data))
