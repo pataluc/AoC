7948afd998d5cd498118962592f923a5c50fef85
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2023-12-18 21:05:25 +0100

    tentative

--- 2023/day12/ex.py
+++ 2023/day12/ex.py
@@ -2,8 +2,8 @@
 from os import path
 import sys
 # from collections import Counter
-import math
-import regex as re
+# import math
+import re
 # import numpy as np
 
 def file_path(file):
@@ -15,10 +15,6 @@ def load(file):
     return open(file_path(file), "r", encoding='utf-8').read().rstrip()
 
 DEBUG = False
-def dprint(*s, end='\n'):
-    """Print function. Prints or not according to DEBUG"""
-    if DEBUG:
-        print(*s, end)
 
 def generate_lines(line, results = []):
     """Compute ex answer"""
@@ -27,47 +23,57 @@ def generate_lines(line, results = []):
     else:
         return [line]
 
-def nb_arrangements(line: str):
+def nb_arrangements(record: str, groups):
     """Compute ex answer"""
-    record, groups = line.split()
-    dprint('gruops', groups)
+
+    if DEBUG: print('groups', groups)
     r = '^\.*%s\.*$' % '\.+'.join(['#' * int(x) for x in groups.split(',')])
-    dprint('regex is: ', r)
-    # dprint('list generee', generate_lines(record))
+    if DEBUG: print('regex is: ', r)
+    # if DEBUG: print('list generee', generate_lines(record))
     result = 0
     for line in generate_lines(record):
         if re.match(r, line):
-            dprint(line, 'matc  hes')
+            if DEBUG: print(line, 'matches')
             result +=1
-    
+
     return result
 
 def ex1(data):
     """Compute ex answer"""
-
     result = 0
 
-    for line in data.split('\n'):
-        dprint("line", line)
-        nb = nb_arrangements(line)
-        dprint("nb", nb)
+    for record, group in [line.split() for line in data.split('\n')]:
+        if DEBUG: print("line", record, group)
+        nb = nb_arrangements(record, group)
+        if DEBUG: print("nb", nb)
         result += nb
     return result
 
     # return sum([nb_arrangements(line) for line in data.split('\n')])
 
-def ex2(data, h, v, times):
+def ex2(data):
     """Compute ex answer"""
-    return 0
+    result = 0
+    for record, group in [line.split() for line in data.split('\n')]:
+        record2, group2 = '?'.join([record]*5), ','.join([group]*5)
+        if DEBUG: print("line", record, group)
+        
+        nb1 = nb_arrangements(record, group)
+        nb2 = nb_arrangements(record2, group2)
+        nb = pow(nb1/nb2, 3) * nb2
 
-# assert len(generate_lines('??.###')) == 4
+        print("nb", nb)
+        result += nb
+        if result > 1 : exit()
+    return result
 
-# assert ex1(load("sample.txt")) == 21
-# print(f'ex1 : {ex1(load("input.txt"))}')
-DEBUG = True
+assert len(generate_lines('??.###')) == 4
 
-assert ex2((load("sample.txt")), 10) == 1030
-assert ex2((load("sample.txt")), 100) == 8410
+assert ex1(load("sample.txt")) == 21
+print(f'ex1 : {ex1(load("input.txt"))}')
 
-print(f'ex2 : {ex2((load("input.txt")), 1000000)}')
 sys.exit()
+assert ex2((load("sample.txt"))) == 525152
+print(f'ex2 : {ex2((load("input.txt")), 1000000)}')
+
+DEBUG = True
