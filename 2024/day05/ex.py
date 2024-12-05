04e9a9411a95671c30653f967728b12487114dda
FOUIN Luc (i-BP) <luc.fouin@i-bp.fr>
2024-12-05 11:32:39 +0100

    improving and linting

--- 2024/day05/ex.py
+++ 2024/day05/ex.py
@@ -5,11 +5,12 @@ from os import path
 import sys
 # from collections import deque
 # import math
-import re
-from colorama import Fore
+# import re
+# from colorama import Fore
 # import numpy as np
 # from heapq import *
 # import networkx as nx
+import functools
 
 def file_path(file):
     """Compute input file path"""
@@ -21,77 +22,66 @@ def load(file):
 
 DEBUG = False
 
+def load_data(data: str) -> tuple:
+    """Loads data as a tuple of list(tuple) and list(list)"""
+    rules, updates = data.split('\n\n')
+
+    rules = [tuple(map(int, rule.split('|'))) for rule in rules.split('\n')]
+    updates = [list(map(int, update.split(','))) for update in updates.split('\n')]
+
+    return rules, updates
+
+def is_update_correct(rules, update):
+    """Check whether an update follows the rules"""
+    index_map = {page: idx for idx, page in enumerate(update)}
+    for x, y in rules:
+        if x in index_map and y in index_map and index_map[x] > index_map[y]:
+            return False
+    return True
+
+
 def ex1(data: str) -> int:
     """Compute ex answer"""
 
-    raw_rules, to_produce = data.split('\n\n')
-
-    # rules = dict()
-    # for rule in raw_rules.split('\n'):
-    #     b, a = tuple(map(int, rule.split('|')))
-    #     if b in rules:
-    #         rules[b].add(a)
-    #     else:
-    #         rules[b] = {a}
-    # print(rules)
-
-    # result = 0
-    # for pages in to_produce.split('\n'):
-    #     pages = list(map(int, pages.split(',')))
-    #     correct = True
-    #     for i in range(len(pages)):
-    #         for j in range(i+1, len(pages)):
-    #             if pages[j] in rules and pages[i] in rules[pages[j]]:
-    #                 correct = False
-    #                 break
-    #         if not correct:
-    #             break
-    #     print("pages are correct: ", pages)
-
-    rules = [r"%s.*%s" % (a, b) for (b, a) in map(lambda r: r.split('|'), raw_rules.split('\n'))]
-    print(rules)
-
-    for pages in to_produce.split('\n'):
-        print(pages)
-        if any(map(lambda rule: re.match(rule, pages), rules)):
-            print("pages are incorrect: ", pages)
-
-
-
-def ex2(data: str) -> int:
-    """Compute ex answer"""
-    grid = [list(line) for line in data.split('\n')]
-    R = len(grid)
-    C = len(grid[0])
+    rules, updates = load_data(data)
 
     result = 0
-    xs = 0
-    for r in range(1, R-1):
-        for c in range(1, C-1):
-            if grid[r][c] == 'A':
-                xs += 1
-
-                for (nw, ne, sw, se) in [
-                    ('M', 'M', 'S', 'S'),
-                    ('M', 'S', 'M', 'S'),
-                    ('S', 'M', 'S', 'M'),
-                    ('S', 'S', 'M', 'M')
-                    ]:
-                    if grid[r-1][c-1] == nw and grid[r-1][c+1] == ne and grid[r+1][c-1] == sw and grid[r+1][c+1] == se:
-                        result += 1
+    for update in updates:
+        if is_update_correct(rules, update):
+            result += int(update[len(update) // 2])
 
     return result
 
+def ex2(data: str) -> int:
+    """Compute ex answer"""
 
+    rules, updates = load_data(data)
+
+    def compare_updates(x, y):
+        """Compare pages based on rules"""
+        if (x, y) in rules:
+            return 1
+        if (y, x) in rules:
+            return -1
+
+        return 0
+
+    result = 0
+    for update in updates:
+        if not is_update_correct(rules, update):
+            update_sorted = sorted(update, key=functools.cmp_to_key(compare_updates))
+
+            result += int(update_sorted[len(update_sorted) // 2])
+
+    return result
 
 assert ex1(load("sample.txt")) == 143
 print(f'ex1 : {ex1(load("input.txt"))}')
 
 
 # DEBUG = True
-assert ex2(load("sample.txt")) == 9
+assert ex2(load("sample.txt")) == 123
 print(f'ex2 : {ex2(load("input.txt"))}')
 
 
 sys.exit()
-
